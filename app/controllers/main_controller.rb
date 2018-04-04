bd41868d3f2eff7100080f47795816e52f3f5f67
Felipe Cano <felipe@acid.cl>
2018-04-04 13:29:14 -0300

    Improve styles for views

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,12 +5,18 @@ class MainController < ApplicationController
       starts_at = params[:date_range][:starts_at].to_time
       ends_at = params[:date_range][:ends_at].to_time
 
+      if starts_at > ends_at
+        starts_at, ends_at = ends_at, starts_at
+      elsif starts_at = ends_at
+        redirect_to root_path and return
+      end
+
       @ufs = Currency.between(starts_at, ends_at, :uf, true)
       @dolares = Currency.between(starts_at, ends_at, :dolar, true)
-     
+
     else
-      redirect_to :back
+      redirect_to root_path and return
     end
   end
-    
+
 end
